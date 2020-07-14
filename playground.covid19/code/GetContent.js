1fddf642d85ba7d53532095666f31895d15653cc
Nam Ho Koh <lucaskoh.lk@gmail.com>
2020-07-13 23:07:19 -0700

    Integrated CDC youtube video links for correct information.

---
+++ playground.covid19/code/GetContent.js
@@ -0,0 +1,49 @@
+const UTIL = require("./lib/util");
+const CONTENT = require("./content");
+const GET_REMOTE = require('./lib/getRemoteContent.js')
+
+// GetContent
+exports.function = function (searchTerm) {
+  //You can replace with a call to a web api - make sure you map api response to content model
+  var content = CONTENT
+  var chosenContent
+
+  // Hard coded searchTerm example to content retrieval from an API
+  if (searchTerm && searchTerm.toLowerCase() == 'dad') {
+    var dadJoke = GET_REMOTE.getDadJoke();
+    
+    // Add a random dad image
+    var dadImages = ['images/dad1.png', 'images/dad2.png', 'images/dad3.png'];
+    var image = dadImages[Math.floor(dadImages.length * Math.random())]
+
+   return {
+      text: dadJoke,
+      image: {
+        url: image
+      }
+    }
+  } else {
+    // Get content from local content.js file 
+    // filter based on searchTerm (note that if you use a web API, then filtering can be done in the web API itself)
+    if (searchTerm) {
+      content = UTIL.findContent(content, searchTerm)
+    }
+
+    //pick a random content
+    if (content.length) {
+      var index = Math.floor(content.length * Math.random())
+      chosenContent = content[index]
+    }
+  }
+  
+  // return content if exists, else null (No Result)
+  if (chosenContent) {
+    return {
+      text: chosenContent.text,
+      image: chosenContent.image,
+      youtuveId: chosenContent.youtubeId
+    }
+  } else {
+    return null;
+  }
+}
---
+++ playground.covid19/code/content.js
@@ -0,0 +1,28 @@
+module.exports = [
+  // Replace content below to customize
+  // tags are optional and used for "find facts about [tag]" etc
+  // image is optional - displayed on UI
+  // START PASTE FROM SPREADSHEET HERE
+  {
+    text: "Key Times to Practive Social Distancing",
+    image: {
+      url: "images/gum.png"
+    },
+    youtubeId: "nOa8wIhQdzo"
+  },
+  {
+    text: "Key Times to Wear a Cloth Face Cover",
+    image: {
+      url: "images/sneeze.png"
+    },
+    youtubeId: "TYgTkJD1BHY"
+  },
+  {
+    text: "I Had COVID-19, But No Symptoms. When Can I Be with Others?",
+    image: {
+      url: "images/sneeze.png"
+    },
+    youtubeId: "tpsOw7kkJSY"
+  }
+  // END PASTE FROM SPREADSHEET HERE
+]
